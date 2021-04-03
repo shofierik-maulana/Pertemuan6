0eb79290cd98765694347a63e0b4df3f6d3d5907
Shofierikmaulana <62019573+shofierik-maulana@users.noreply.github.com>
2021-04-03 07:25:30 -0700

    Add files via upload

---
+++ pert6_latihan2[1].php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+ $x=array("one","two","three"); 
+foreach ($x as $value) 
+ { 
+ echo $value . "<br />"; 
+ }
+ ?>
+</body>
+</html>
---
+++ pert6_latihan3[1].php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+ $UsiaKaryawan["Lisa"] = "28"; 
+$UsiaKaryawan["Jack"] = "16"; 
+$UsiaKaryawan["Ryan"] = "35"; 
+$UsiaKaryawan["Rachel"] = "46"; 
+$UsiaKaryawan["Grace"] = "34"; 
+foreach($UsiaKaryawan as $Nama => $umur) 
+{ 
+echo "Nama Karyawan: $Nama, Usia: $umur"." th <br>"; 
+}
+?>
+</body>
+</html>
